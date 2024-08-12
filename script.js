dd790516337bcec07d9271e973f42e971104db80
Subhan <subhan.tinwala@gmail.com>
2024-08-12 23:26:47 +0500

    Write a funtion to play a round

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 function getComputerChoice() {
   const choices = ["ROCK", "PAPER", "SCISSORS"];
-  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-  return computerChoice;
+  return choices[Math.floor(Math.random() * choices.length)];
+  // return computerChoice;
 }
 
 function getPlayerChoice() {
-  const playerChoice = prompt(
+  return prompt(
     "Please enter your choice (ROCK, PAPER, SCISSORS.)"
-  );
-  return playerChoice.toUpperCase();
+  ).toUpperCase();
+  //   return playerChoice.toUpperCase();
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) console.log("It's a tie!");
+  else if (
+    (playerChoice === "ROCK" && computerChoice === "PAPER") ||
+    (playerChoice === "SCISSORS" && computerChoice === "ROCK") ||
+    (playerChoice === "PAPER" && computerChoice === "SCISSORS")
+  ) {
+    console.log("Computer wins this round!");
+    computerScore++;
+    console.log(
+      "SCORE: Player = " + playerScore + " | Computer = " + computerScore
+    );
+  } else {
+    console.log("Player wins this round!");
+    playerScore++;
+    console.log(
+      "SCORE: Player = " + playerScore + "| Computer = " + computerScore
+    );
+  }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
