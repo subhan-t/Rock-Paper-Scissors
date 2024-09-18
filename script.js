d50e83165f43cc32b620c6f21c6edc5067dce525
Subhan <subhan.tinwala@gmail.com>
2024-09-19 00:24:18 +0500

    Rewrite playRound to add UI and scoreboard

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
-function getComputerChoice() {
-  const choices = ["ROCK", "PAPER", "SCISSORS"];
-  return choices[Math.floor(Math.random() * choices.length)];
-  // return computerChoice;
-}
-
-function getPlayerChoice() {
-  return prompt(
-    "Please enter your choice (ROCK, PAPER, SCISSORS.)"
-  ).toUpperCase();
-  //   return playerChoice.toUpperCase();
-}
-
 function playGame() {
   let playerScore = 0;
   let computerScore = 0;
 
-  for (let round = 0; round < 5; round++) {
-    playRound(getPlayerChoice(), getComputerChoice());
+  function getComputerChoice() {
+    const choices = ["ROCK", "PAPER", "SCISSORS"];
+    return choices[Math.floor(Math.random() * choices.length)];
   }
-  if (playerScore === computerScore) console.log("The game is a draw!");
-  else if (playerScore > computerScore) console.log("Player wins!");
-  else console.log("Computer wins!");
+
+  let playerSelection = document.querySelectorAll("button");
+  playerSelection.forEach((button) => {
+    button.addEventListener("click", () => {
+      playRound(button.id, getComputerChoice());
+    });
+  });
 
   function playRound(playerChoice, computerChoice) {
-    console.log(
-      "Player chose: " + playerChoice + "| Computer chose: " + computerChoice
-    );
-    if (playerChoice === computerChoice) console.log("It's a tie!");
-    else if (
+    let choices =
+      "Player chose: " + playerChoice + "| Computer chose: " + computerChoice;
+
+    if (playerChoice === computerChoice) {
+      let roundResult = "It's a tie!";
+      document.querySelector("div.round-result").textContent = roundResult;
+    } else if (
       (playerChoice === "ROCK" && computerChoice === "PAPER") ||
       (playerChoice === "SCISSORS" && computerChoice === "ROCK") ||
       (playerChoice === "PAPER" && computerChoice === "SCISSORS")
     ) {
-      console.log("Computer wins this round!");
+      let roundResult = "Computer wins this round!";
       computerScore++;
-      console.log(
-        "SCORE: Player = " + playerScore + " | Computer = " + computerScore
-      );
+      document.querySelector("div.round-result").textContent = roundResult;
+      let score =
+        "SCORE: Player = " + playerScore + " | Computer = " + computerScore;
+      document.querySelector("div.score").textContent = score;
+
+      if (playerScore < 5 && computerScore === 5) {
+        let result = "Computer wins!";
+        document.querySelector("div.result").textContent = result;
+      }
     } else {
-      console.log("Player wins this round!");
+      let roundResult = "Player wins this round!";
       playerScore++;
-      console.log(
-        "SCORE: Player = " + playerScore + "| Computer = " + computerScore
-      );
+      document.querySelector("div.round-result").textContent = roundResult;
+      let score =
+        "SCORE: Player = " + playerScore + " | Computer = " + computerScore;
+      document.querySelector("div.score").textContent = score;
+
+      if (playerScore === 5 && computerScore < 5) {
+        let result = "Player wins!";
+        document.querySelector("div.result").textContent = result;
+      }
     }
+    document.querySelector("div.choices").textContent = choices;
   }
 }
 
